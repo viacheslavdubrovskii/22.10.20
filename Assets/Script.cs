707e0f5d18c38383dd1e1a081a10fb2c1b47fd99
Lrunner <viacheslav.dubrovskii.@mail.ru>
2020-10-22 14:00:39 +0300

    added 22.10 game

---
+++ Assets/Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script : MonoBehaviour
+{
+    public GameObject obj;
+
+    private Light MyLight;
+
+    private void Start()
+    {
+        MyLight = GetComponent<Light>();
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            MyLight.enabled = !MyLight.enabled;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Destroy(obj);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            obj.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+            obj.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
