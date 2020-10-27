c114a437f59f2b33d5d58b66f838d0de7ecbf946
Lrunner <viacheslav.dubrovskii.@mail.ru>
2020-10-27 23:41:11 +0300

    22.10.20 added

---
+++ 22.10.20/Assets/Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("Hi Awake");
+    }
+    void Start()
+    {
+        print("Hi!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
